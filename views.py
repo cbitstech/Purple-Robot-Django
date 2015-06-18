b40c42fbab11743e3ebc138ac32065fd4c26f56a
Chris J. Karr <chris@audacious-software.com>
2015-06-18 16:41:45 -0500

    Fixing imports

--- views.py
+++ views.py
@@ -3,6 +3,7 @@ import hashlib
 import datetime
 
 from django.contrib.admin.views.decorators import staff_member_required
+from django.core.urlresolvers import reverse
 from django.http import HttpResponse, UnreadablePostError
 from django.shortcuts import render_to_response, get_object_or_404, redirect
 from django.template import RequestContext
