875cf58d2dddb5a83a5c3f17da21462243d29c41
Chris Karr <github@audacious-software.com>
2015-08-04 15:47:01 -0500

    Making FitBit beta visualization more robust to missing data

--- formatters/services_fitbitbetaprobe.py
+++ formatters/services_fitbitbetaprobe.py
@@ -18,41 +18,47 @@ def visualize(probe_name, readings):
     for reading in readings:
         item = json.loads(reading.payload)
         
-        for i in range(0, len(item['HEART'])):
-            plot = { 'x': item['HEART_TIMESTAMPS'][i] / 1000, 'y': item['HEART'][i] }
+        if 'HEART' in item:
+            for i in range(0, len(item['HEART'])):
+                plot = { 'x': item['HEART_TIMESTAMPS'][i] / 1000, 'y': item['HEART'][i] }
             
-            heart.append(plot)
+                heart.append(plot)
 
-        for i in range(0, len(item['STEPS'])):
-            plot = { 'x': item['STEP_TIMESTAMPS'][i] / 1000, 'y': item['STEPS'][i] }
+        if 'STEPS' in item:
+            for i in range(0, len(item['STEPS'])):
+                plot = { 'x': item['STEP_TIMESTAMPS'][i] / 1000, 'y': item['STEPS'][i] }
             
-            steps.append(plot)
+                steps.append(plot)
 
-        for i in range(0, len(item['FLOORS'])):
-            plot = { 'x': item['FLOORS_TIMESTAMPS'][i] / 1000, 'y': item['FLOORS'][i] }
+        if 'FLOORS' in item:
+            for i in range(0, len(item['FLOORS'])):
+                plot = { 'x': item['FLOORS_TIMESTAMPS'][i] / 1000, 'y': item['FLOORS'][i] }
             
-            floors.append(plot)
+                floors.append(plot)
 
-        for i in range(0, len(item['ELEVATION'])):
-            plot = { 'x': item['ELEVATION_TIMESTAMPS'][i] / 1000, 'y': item['ELEVATION'][i] }
+        if 'ELEVATION' in item:
+            for i in range(0, len(item['ELEVATION'])):
+                plot = { 'x': item['ELEVATION_TIMESTAMPS'][i] / 1000, 'y': item['ELEVATION'][i] }
             
-            elevation.append(plot)
+                elevation.append(plot)
 
-        for i in range(0, len(item['CALORIES'])):
-            plot = { 'x': item['CALORIES_TIMESTAMPS'][i] / 1000, 'y': item['CALORIES'][i] }
+        if 'CALORIES' in item:
+            for i in range(0, len(item['CALORIES'])):
+                plot = { 'x': item['CALORIES_TIMESTAMPS'][i] / 1000, 'y': item['CALORIES'][i] }
             
-            calories.append(plot)
+                calories.append(plot)
 
-        for i in range(0, len(item['DISTANCE'])):
-            plot = { 'x': item['DISTANCE_TIMESTAMPS'][i] / 1000, 'y': item['DISTANCE'][i] }
+        if 'DISTANCE' in item:
+            for i in range(0, len(item['DISTANCE'])):
+                plot = { 'x': item['DISTANCE_TIMESTAMPS'][i] / 1000, 'y': item['DISTANCE'][i] }
             
-            distance.append(plot)
+                distance.append(plot)
 
-        heart.sort(key=lambda o: o['x'])
-        steps.sort(key=lambda o: o['x'])
-        floors.sort(key=lambda o: o['x'])
-        elevation.sort(key=lambda o: o['x'])
-        calories.sort(key=lambda o: o['x'])
-        distance.sort(key=lambda o: o['x'])
+    heart.sort(key=lambda o: o['x'])
+    steps.sort(key=lambda o: o['x'])
+    floors.sort(key=lambda o: o['x'])
+    elevation.sort(key=lambda o: o['x'])
+    calories.sort(key=lambda o: o['x'])
+    distance.sort(key=lambda o: o['x'])
      
     return render_to_string('visualization_fitbit.html', { 'probe_name': probe_name, 'distance': json.dumps(distance), 'heart': json.dumps(heart), 'steps': json.dumps(steps), 'floors': json.dumps(floors), 'elevation': json.dumps(elevation), 'calories': json.dumps(calories) })
